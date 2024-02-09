9beabbdaaf293c5325c0cdec192bcc73332ee0ae
AKorotkov07 <153372700+AKorotkov07@users.noreply.github.com>
2024-02-09 14:09:44 +0300

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,64 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Employee> employeeList = parseXML("data.xml");
+        String json = listToJson(employeeList);
+        writeString(json, "data2.json");
+    }
+
+    public static List<Employee> parseXML(String filePath) {
+        List<Employee> employeeList = new ArrayList<>();
+
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(filePath);
+
+            Element root = document.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = element.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+
+                    Employee employee = new Employee(0, firstName, lastName, country, age);
+                    employeeList.add(employee);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return employeeList;
+    }
+
+    private static String listToJson(List<Employee> list) {
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(list, listType);
+    }
+
+    private static void writeString(String content, String fileName) {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
